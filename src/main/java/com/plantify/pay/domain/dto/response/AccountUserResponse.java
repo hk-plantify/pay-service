8a379e1f1ec962ebabb37ecb9b090ea5c174d857
zzoo0123 <0123suh@gmail.com>
2024-11-19 16:01:55 +0900

    feat: AccountUser Dto

---
+++ src/main/java/com/plantify/pay/domain/dto/response/AccountUserResponse.java
@@ -0,0 +1,29 @@
+package com.plantify.pay.domain.dto.response;
+
+
+import com.plantify.pay.domain.entity.Account;
+import com.plantify.pay.domain.entity.AccountStatus;
+
+import java.time.LocalDateTime;
+
+public record AccountUserResponse(
+        Long accountId,
+        Long accountNum,
+        String bankName,
+        AccountStatus accountStatus,
+        String accountHolder,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+    public static AccountUserResponse from(Account account) {
+        return new AccountUserResponse(
+                account.getAccountId(),
+                account.getAccountNum(),
+                account.getBankName(),
+                account.getAccountStatus(),
+                account.getAccountHolder(),
+                account.getCreatedAt(),
+                account.getUpdatedAt()
+        );
+    }
+}
