0d0319e5abf90a8bcdaa09fc2ad32337c35d1f48
zzoo0123 <0123suh@gmail.com>
2024-11-19 16:31:05 +0900

    feat: PayErrorCode 수정

--- src/main/java/com/plantify/pay/global/exception/errorcode/PayErrorCode.java
+++ src/main/java/com/plantify/pay/global/exception/errorcode/PayErrorCode.java
@@ -7,11 +7,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum PayErrorCode implements ErrorCode {
 
-    PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "결제를 찾을 수 없습니다."),
-    INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
     UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다."),
-    INVALID_PAYMENT_STATUS(HttpStatus.BAD_REQUEST, "유효하지 않은 결제 상태입니다."),
-    DUPLICATE_PAYMENT(HttpStatus.CONFLICT, "중복된 결제 요청입니다.");
+    PAY_NOT_FOUND(HttpStatus.NOT_FOUND, "결제를 찾을 수 없습니다."),
+    INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
+    PAY_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 생성된 결제가 존재합니다."),
+    INVALID_PAY_STATUS(HttpStatus.BAD_REQUEST, "유효하지 않은 결제 상태입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
