b17162fa9d74b5f05489c246de467c63c98cf92f
zzoo0123 <0123suh@gmail.com>
2024-11-19 16:53:13 +0900

    feat: PaySettlementUser Dto

--- src/main/java/com/plantify/pay/domain/dto/request/PSettlementRequest.java
+++
@@ -1,8 +0,0 @@
-package com.plantify.pay.domain.dto.request;
-
-public record PSettlementRequest(
-        Long payId,
-        String transactionType,
-        Long transactionAmount,
-        Long balanceAfter
-) {}
