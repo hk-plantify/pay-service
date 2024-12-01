ff3b2a3d8a97e104f41b0979d7a9a52ffe09c4ea
zzoo0123 <0123suh@gmail.com>
2024-12-02 02:30:56 +0900

    feat: global response, errorcode 수정

--- src/main/java/com/plantify/pay/global/exception/errorcode/PayErrorCode.java
+++ src/main/java/com/plantify/pay/global/exception/errorcode/PayErrorCode.java
@@ -8,10 +8,14 @@ import org.springframework.http.HttpStatus;
 public enum PayErrorCode implements ErrorCode {
 
     UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다."),
-    PAY_NOT_FOUND(HttpStatus.NOT_FOUND, "결제를 찾을 수 없습니다."),
-    INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
-    PAY_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 생성된 결제가 존재합니다."),
-    INVALID_PAY_STATUS(HttpStatus.BAD_REQUEST, "유효하지 않은 결제 상태입니다.");
+    PAY_NOT_FOUND(HttpStatus.NOT_FOUND, "페이를 찾을 수 없습니다."),
+    PAY_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 생성된 페이가 존재합니다."),
+    INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다"),
+    INVALID_PAY_INPUT(HttpStatus.BAD_REQUEST, "충전 금액은 0보다 커야 합니다."),
+    INVALID_CHARGE_UNIT(HttpStatus.BAD_REQUEST, "충전 금액은 10,000원 단위로만 가능합니다."),
+    INVALID_REFUND_AMOUNT(HttpStatus.BAD_REQUEST, "환불 금액이 결제 금액을 초과할 수 없습니다."),
+    CONCURRENT_UPDATE(HttpStatus.CONFLICT, "동시에 처리 중인 요청이 있습니다."),
+    DUPLICATE_TRANSACTION(HttpStatus.CONFLICT, "이미 진행 중인 결제가 존재합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/plantify/pay/global/exception/errorcode/PointErrorCode.java
+++ src/main/java/com/plantify/pay/global/exception/errorcode/PointErrorCode.java
@@ -10,7 +10,8 @@ public enum PointErrorCode implements ErrorCode {
     POINT_NOT_FOUND(HttpStatus.NOT_FOUND, "포인트 정보를 찾을 수 없습니다."),
     INSUFFICIENT_POINTS(HttpStatus.BAD_REQUEST, "포인트 잔액이 부족합니다."),
     UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "포인트 접근 권한이 없습니다."),
-    INVALID_POINT_OPERATION(HttpStatus.BAD_REQUEST, "유효하지 않은 포인트 작업 요청입니다.");
+    INVALID_POINT_OPERATION(HttpStatus.BAD_REQUEST, "유효하지 않은 포인트 작업 요청입니다."),
+    CONCURRENT_UPDATE(HttpStatus.CONFLICT, "포인트 정보 업데이트 중 충돌이 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
