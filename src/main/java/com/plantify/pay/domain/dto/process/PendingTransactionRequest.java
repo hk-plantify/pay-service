c5d5faeb90df5775d70de4a4c6990fba52c6a7d3
seunzu <0123suh@gmail.com>
2025-12-25 01:34:09 +0900

    refactor: 사용자 식별을 SecurityContext 기준으로 통합

--- src/main/java/com/plantify/pay/domain/dto/process/PendingTransactionRequest.java
+++ src/main/java/com/plantify/pay/domain/dto/process/PendingTransactionRequest.java
@@ -1,7 +1,6 @@
 package com.plantify.pay.domain.dto.process;
 
 public record PendingTransactionRequest(
-        Long userId,
         Long sellerId,
         String orderName,
         Long amount,
